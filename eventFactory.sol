7c6d95d285c20f12fbf17baad3d25b4600a7e067
null <wasimthoufiq21@gmail.com>
2022-05-23 11:34:19 +0530

    my changes for event log contract

---
+++ eventFactory.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.8.4;
+
+// This contract
+// 1) is a factory of eventGame contracts
+// 2) records every event created
+
+import "./eventGame.sol";
+
+contract eventFactory {
+    address immutable s_logAddress;
+    uint256 s_nextId;
+
+    constructor(address _logAddress) {
+        s_logAddress = _logAddress;
+        s_nextId = 0;
+    }
+
+    // deploy a new contract for the event and log it
+    function createEventGame(
+        string memory _eventName,
+        uint256 _numberOfTickets,
+        uint256 _ticketPrice
+    ) external {
+        eventLog log = eventLog(s_logAddress);
+        eventGame game = new eventGame(s_logAddress, msg.sender, s_nextId);
+        s_nextId += 1;
+        log._logEvent(
+            s_nextId,
+            address(game),
+            msg.sender,
+            _eventName,
+            _numberOfTickets,
+            _ticketPrice
+        );
+    }
+}
---
+++ eventLog.sol
@@ -0,0 +1,99 @@
+pragma solidity 0.8.4;
+
+//This contract is a log of the created events
+
+contract eventLog {
+    // all events ever created are stored in these structures
+    uint256 s_numberOfEvents;
+    uint256[] s_eventIds;
+    mapping(uint256 => Event) s_events; // s_eventId => Event
+
+    // every user events are stored as userAddress => Event
+    mapping(address => Event[]) s_registeredEvents;
+    mapping(address => Event[]) s_createdEvents;
+
+    // the winners of each event are stored as s_eventId => userAddress => true
+    mapping(uint256 => mapping(address => bool)) s_winners;
+
+    struct Event {
+        address eventGameAddress;
+        address eventOwner;
+        string eventName;
+        uint256 numberOfTickets;
+        uint256 ticketPrice;
+        bool isOpen;
+    }
+
+    constructor() {
+        s_numberOfEvents = 0;
+    }
+
+    function _logEvent(
+        uint256 _eventId,
+        address _eventGameAddress,
+        address _eventOwner,
+        string memory _eventName,
+        uint256 _numberOfTickets,
+        uint256 _ticketPrice
+    ) external {
+        s_events[_eventId] = Event(
+            _eventGameAddress,
+            _eventOwner,
+            _eventName,
+            _numberOfTickets,
+            _ticketPrice,
+            true
+        );
+        s_numberOfEvents += 1;
+        s_eventIds.push(_eventId);
+    }
+    modifier onlyOwner(uint256 _eventId) {
+        Event memory old_event = s_events[_eventId];
+        require(old_event.eventOwner == msg.sender, "Only owner can update");
+        _;
+    }
+
+    function _updateName(uint256 _eventId, string memory _newName) onlyOwner(_eventId) external {
+        s_events[_eventId].eventName = _newName;
+    }
+
+    function _updateTickets(uint256 _eventId, uint256 _newTickets) onlyOwner(_eventId) external {
+        s_events[_eventId].numberOfTickets = _newTickets;
+    }
+
+    function _updatePrice(uint256 _eventId, uint256 _newPrice) onlyOwner(_eventId) external {
+        s_events[_eventId].ticketPrice = _newPrice;
+    }
+
+    function _closeEvent(uint256 _eventId) onlyOwner(_eventId) external {
+        s_events[_eventId].isOpen = false;
+    }
+
+    function getEvent(uint256 _eventId) public view returns (Event memory) {
+        return s_events[_eventId];
+    }
+
+    function getOpenEvents() public view returns (Event[] memory) {
+        uint256 availableLength = 0;
+        for (uint256 i = 0; i < s_numberOfEvents; i++){
+                    if (s_events[i].isOpen == true){
+                        availableLength += 1;
+                    }
+        }
+
+        uint256 currentIndex = 0;
+        Event[] memory openEvents = new Event[](availableLength);
+        for (uint256 i = 0; i < s_numberOfEvents; i++){
+                    if (s_events[i].isOpen == true){
+                        Event storage currentItem = s_events[i];
+                        openEvents[currentIndex] = currentItem;
+                        currentIndex += 1;
+                    }
+        }
+        return openEvents;
+    }
+
+    function _addWinner(uint256 _eventId, address _winner) external {
+        s_winners[_eventId][_winner] = true;
+    }
+}
