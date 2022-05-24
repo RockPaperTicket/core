72a18de8065b958c2b651db77eeffc838f02feea
JK <53575042+JHKaller@users.noreply.github.com>
2022-05-24 20:09:46 +0200

    Delete scripts directory

--- scripts/deploy_contract.py
+++
@@ -1,8 +0,0 @@
-from scripts.helpful_scripts import get_account
-from brownie import TicketMinter
-
-def main():
-    account = get_account()
-    TicketMinter.deploy(100, "JK", {"from": account})
-
-
