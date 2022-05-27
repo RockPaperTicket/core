7a6abc89125978c1ba63471ab6ad76ceeca91f95
JHKaller <john.kaller@hotmail.de>
2022-05-27 13:56:24 +0200

    eventIDfunction add

--- contracts/TicketMinter.sol
+++ contracts/TicketMinter.sol
@@ -10,7 +10,7 @@ interface IeventGame {
     function _isWinner(uint256 _eventId, address _userAddress) external view returns (bool);
     function getNumberOfTickets(uint256 _eventId) external view returns (uint256);
     function getEventName(uint256 _eventId) external view returns (string memory);
-    function _eventId() external view returns (uint256);
+    function _eventId() external returns (uint256);
 }
 
 contract TicketMinter is ERC721URIStorage{
@@ -27,20 +27,20 @@ contract TicketMinter is ERC721URIStorage{
     }
 
     //instead of eventLog address, I simply pass in eventaddress
-    function mintTicket(address _eventAddress, uint256 _eventId) public {
+    function mintTicket(address _eventAddress) public{
         
         //set new tokenID
         newTokenId = _tokenIds.current();
 
         // access necessary information for the ticket
-        //uint256 _eventId = IeventGame(_eventAddress)._eventId();
+        uint256 _eventId = IeventGame(_eventAddress)._eventId();
         uint256 numberOfTickets = IeventGame(_eventAddress).getNumberOfTickets(_eventId);
         string memory eventName = IeventGame(_eventAddress).getEventName(_eventId);
 
         //check all requirements
         require(newTokenId <= numberOfTickets, "All Tickets have been sold out!");
         require(IeventGame(_eventAddress)._isWinner(_eventId ,msg.sender) == true, "You are not qualified to receive a ticket.");
-        //require(balanceOf(msg.sender) == 0, 'Each player may only own one ticket');
+        require(balanceOf(msg.sender) == 0, 'Each player may only own one ticket');
 
         //define finalSVG name
         string memory finalSvg = string(abi.encodePacked(baseSvg1, eventName, "</text>", baseSvg2, newTokenId.toString(), "</text>", baseSvg3, numberOfTickets.toString(), "</text></svg>"));
